2853ae8d50fc6e7ffe385589abee229cffc77db0
GFK\noklam.chan <noklam.chan@gfk.com>
2018-08-01 17:26:15 +0800

    created the first model, python manage.py makemigration

--- airlines/flights/models.py
+++ airlines/flights/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+length = 64
+class Flight(models.Model):
+    origin = models.CharField(max_length=length )
+    destinations = models.CharField(max_length=length)
+    duration = models.CharField(max_length=length)
+    pass

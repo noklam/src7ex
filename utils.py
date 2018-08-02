912b34988c50f7184e5f4a40e08bf707f4966ed0
GFK\noklam.chan <noklam.chan@gfk.com>
2018-08-02 19:07:58 +0800

    modify utils.py

--- utils.py
+++ utils.py
@@ -5,5 +5,20 @@ python manage.py migrate # To migrate models change
 python manage.py sqlmigrate flights 0001 # See the actual SQL details
 python manage.py shell # also you to interact in a shell mode
 
+from  flights.models import Flight
+f = Flight(origin="New York", destination="London", duration=420)
+f.save()
+Flight.objects.all()
+
+from flights.models import Airport, Flight
+jfk = Airport(code="JFK", city="NY City")
+lhr = Airport(code="LHR", city="London")
+jfk.save()
+lhr.save()
+f = Flight(origin=jfk, destination=lhr, duration=415)
+f.save()
+
 In apps.py isnert class FlightsConfig(AppConfig) name ='flights'
 In settings.py, add "flights.apps.FlightConfig"
+
+python manage.py createsuperuser

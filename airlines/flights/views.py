05221c49633ca68f063aff61449eb94424ac6620
noklam <noklam.chan@gfk.com>
2018-08-03 16:51:07 +0800

    Add template and book route

--- airlines/flights/views.py
+++ airlines/flights/views.py
@@ -1,13 +1,12 @@
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.shortcuts import render
-from .models import Flight
-
+from .models import Flight, Passenger
+from django.urls import reverse
 # Create your views here.
 def index(request):
     context = {
         "flights": Flight.objects.all()
         }
-
     return render(request, "flights/index.html", context)
 
 def flight(request, flight_id):
@@ -17,7 +16,25 @@ def flight(request, flight_id):
         raise Http404("Flight does not exist, please check again.")
     context = {
         "flight": flight,
-        "passengers": flight.passengers.all()    }
+        "passengers": flight.passengers.all(),
+        "non_passengers": Passenger.objects.exclude(flights=flight).all()
+        }
     return render(request, "flights/flight.html", context)
+
+def book(request, flight_id):
+    try:
+        passenger_id = int(request.POST["passenger"])
+        passenger = Passenger.objects.get(pk=passenger_id)
+        flight  = Flight.objects.get(pk=flight_id)
+    except KeyError:
+        return render(request, "flights/error.html", {"message": "No selection"})
+    except Flight.DoesNotExist:
+        return render(request, "flights/error.html", {"message": "No Flight"})
+    except Passenger.DoesNotExist:
+        return render(request, "flights/error.html", {"message": "No Passenger"})
+
+
+    passenger.flights.add(flight)
+    return HttpResponseRedirect(reverse("flight", args=(flight_id,))) # lookup flights/urls urlpatterns
 
     
